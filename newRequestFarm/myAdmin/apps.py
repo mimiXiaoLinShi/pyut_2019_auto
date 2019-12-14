20080cc4a8312b0e2447efb6a307498878445f11
xiaohei <manger@163.com>
2019-12-14 15:20:44 +0800

    'deletenumber'

--- newRequestFarm/myAdmin/apps.py
+++ newRequestFarm/myAdmin/apps.py
@@ -5,10 +5,3 @@ class MyadminConfig(AppConfig):
     name = 'myAdmin'
 
 
-class nihao:
-    def __init__(self):
-        self.nihao = 'nihao'
-
-class testnumber:
-    def __init__(self):
-        number = 'test_number_mi_01'
