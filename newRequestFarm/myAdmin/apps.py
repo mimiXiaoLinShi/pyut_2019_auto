76c3f1447b5eb57aad3acc14044a92a06664914f
xiaohei <manger@163.com>
2019-12-14 15:19:03 +0800

    'outest2'

--- newRequestFarm/myAdmin/apps.py
+++ newRequestFarm/myAdmin/apps.py
@@ -8,3 +8,7 @@ class MyadminConfig(AppConfig):
 class nihao:
     def __init__(self):
         self.nihao = 'nihao'
+
+class testnumber:
+    def __init__(self):
+        number = 'test_number_mi_01'
